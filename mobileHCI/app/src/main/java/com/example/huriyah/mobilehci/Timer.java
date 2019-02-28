2ce46c34ea5f36dbe41433b761730d044587a77d
Amy Gillies <2254790g@student.gla.ac.uk>
2019-02-28 11:59:08 +0000

    GPS - not working

--- mobileHCI/app/src/main/java/com/example/huriyah/mobilehci/Timer.java
+++ mobileHCI/app/src/main/java/com/example/huriyah/mobilehci/Timer.java
@@ -1,17 +1,47 @@
 package com.example.huriyah.mobilehci;
 
-import android.content.Intent;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.SystemClock;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class Timer extends AppCompatActivity {
 
     private Chronometer chronometer;
     private long pauseOffset;
     private boolean operational;
+
+    private LocationManager locationManager = null;
+    private MyLocationListener locationListener = null;
+
+    private double longitude;
+    private double latitude;
+
+
+    private Boolean displayGpsStatus() {
+        ContentResolver contentResolver = getBaseContext().getContentResolver();
+        boolean gpsStatus = Settings.Secure.isLocationProviderEnabled(contentResolver, LocationManager.GPS_PROVIDER);
+
+        if (gpsStatus) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +50,8 @@ public class Timer extends AppCompatActivity {
 
         chronometer = findViewById(R.id.chronometer);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
     }
 
     public void startChronometer(View v) {
@@ -27,6 +59,28 @@ public class Timer extends AppCompatActivity {
             chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
             chronometer.start();
             operational = true;
+
+            // implement GPS to start here
+            boolean flag = displayGpsStatus();
+            if (flag) {
+                locationListener = new MyLocationListener();
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+                longitude = locationListener.getLongitude();
+                latitude = locationListener.getLatitude();
+                TextView longitudeshow = (TextView)findViewById(R.id.longitudeshow);
+                longitudeshow.setText((int) longitude);
+            }
+            }
         }
     }
 
@@ -44,6 +98,22 @@ public class Timer extends AppCompatActivity {
             pauseOffset = 0;
         }
     }
+
+    protected void alertbox(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your Device's GPS is disabled, you won't be able to view your speed!")
+                .setCancelable(false)
+                .setTitle("** GPS Status **")
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     /*
     public void onClick(View view){
         if(view == b){
@@ -53,4 +123,41 @@ public class Timer extends AppCompatActivity {
         }
 
     } */
+}
+
+class MyLocationListener implements LocationListener {
+
+    private double longitude;
+    private double latitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        longitude = loc.getLongitude();
+        latitude = loc.getLatitude();
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
 }
